c69e8057c0c1c4b9bace0a17e4ec97b4d42882c2
saihaj <44710980+saihaj@users.noreply.github.com>
2020-06-29 17:15:08 -0500

    feat: get list of all events
    related #5

--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,16 @@
 import { Router } from 'express'
+import { EventSchema } from '../models/schemas'
+import { FormatDateTime, ValidateDateTime, GetItemById, DnE } from '../lib/utils'
 
 const app = Router()
 
-app.get( '/', ( _, res ) => (
-  res.json( { message: 'EVENTS ROUTE' } )
-) )
+// Get list of all events
+app.get( '/', async ( _, res, next ) => {
+  try {
+    const events = await EventSchema.find().select( '-__v' )
+    return res.json( events )
+  } catch ( err ) { return next( err ) }
+} )
+
 
 export default app
