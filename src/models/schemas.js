a3437adfc056b8590de356d816cc2df8a6be3229
saihaj <44710980+saihaj@users.noreply.github.com>
2020-07-28 15:46:57 -0500

    feat: add user schema to DB
    related Route: Users #7

--- src/models/schemas.js
+++ src/models/schemas.js
@@ -1,101 +1,102 @@
-import mongoose from 'mongoose'
+import mongoose, { Schema } from 'mongoose'
+import 'mongoose-type-email'
+
 import { Timestamp } from '../lib/utils'
 
 export const BlogSchema = mongoose.model(
   'Blog',
   new mongoose.Schema( {
-    // Link it to USER
     author: {
-      type: String,
+      type: Schema.Types.ObjectId,
+      ref: 'User',
       required: true,
     },
-    content: {
-      type: String,
-      required: true,
-    },
-    title: {
-      type: String,
-      required: true,
-    },
-    dateUploaded: {
-      type: Date,
-      default: Timestamp(),
-    },
-    dateUpdated: {
-      type: Date,
-      default: Timestamp(),
-    },
-    datePublished: {
-      type: Date,
-      default: Timestamp(),
-    },
-    language: {
-      type: String,
-      default: 'en',
-    },
-    tags: {
-      type: [ String ],
-    },
-    category: {
-      type: [ String ],
-    },
-    published: {
-      type: Boolean,
-      default: true,
-    },
+    content: { type: String, required: true },
+    title: { type: String, required: true },
+    dateUploaded: { type: Date, default: Timestamp() },
+    dateUpdated: { type: Date, default: Timestamp() },
+    datePublished: { type: Date, default: Timestamp() },
+    language: { type: String, default: 'en' },
+    tags: [ String ],
+    category: [ String ],
+    published: { type: Boolean, default: true },
   } ),
 )
 
 export const EventSchema = mongoose.model(
   'Events',
   new mongoose.Schema( {
-    // Link it to USER
     organizer: {
-      type: String,
+      type: Schema.Types.ObjectId,
+      ref: 'User',
       required: true,
     },
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    startDate: {
-      type: Date,
-      required: true,
-    },
-    endDate: {
-      type: Date,
-      required: true,
-    },
-    location: {
-      type: String,
-      required: true,
-    },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
+    location: { type: String, required: true },
   } ),
 )
 
 export const ProjectSchema = mongoose.model(
   'Projects',
   new mongoose.Schema( {
-    // Link it to USERS
     contributors: [ {
-      type: String,
+      type: Schema.Types.ObjectId,
+      ref: 'User',
       required: true,
     } ],
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    createdDate: { type: Date, required: true },
+  } ),
+)
+
+// Usernames for different social platforms
+const SocialAccountsSchema = {
+  github: String,
+  reddit: String,
+  discord: String,
+  steam: String,
+  twitch: String,
+  twitter: String,
+  facebook: String,
+  instagram: String,
+  linkedIn: String,
+}
+
+export const UserSchema = mongoose.model(
+  'Users',
+  new mongoose.Schema( {
     name: {
       type: String,
       required: true,
     },
-    description: {
-      type: String,
-      required: true,
+    login: {
+      type: Schema.Types.ObjectId,
+      ref: 'Auth',
     },
-    createdDate: {
-      type: Date,
+    socialAccounts: SocialAccountsSchema,
+    organization: String,
+  } ),
+)
+
+export const AuthSchema = mongoose.model(
+  'Auth',
+  new mongoose.Schema( {
+    email: {
+      type: mongoose.SchemaTypes.Email,
       required: true,
+      unique: true,
+      index: true,
+    },
+    password: { type: String, required: true },
+    role: { type: String, required: true },
+    // one to one relation with users
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
     },
   } ),
 )
