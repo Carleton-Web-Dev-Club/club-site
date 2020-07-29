ae26ab9f0def7e03e429fcab4db81d2a5093a3cf
saihaj <44710980+saihaj@users.noreply.github.com>
2020-07-29 15:26:49 -0500

    feat: ability to get all users and register a user
    related #7

---
+++ src/lib/consts.js
@@ -0,0 +1,5 @@
+export const USER_ROLES = {
+  basic: 'Basic',
+  moderator: 'Moderator',
+  admin: 'Admin',
+}
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,58 @@
 import { Router } from 'express'
+import bcrypt from 'bcryptjs'
+import { omit } from 'lodash'
+import { AuthSchema, UserSchema } from '../models/schemas'
+import { USER_ROLES } from '../lib/consts'
 
 const app = Router()
 
-app.get( '/', ( _, res ) => (
-  res.json( { message: 'USERS ROUTE' } )
-) )
+// Get all users
+app.get( '/', async ( _, res, next ) => {
+  try {
+    const users = await UserSchema
+      .find()
+      .select( '-__v' )
+      .populate( 'login', '-password -user -__v' )
+
+    return res.json( users )
+  } catch ( err ) { return next( err ) }
+} )
+
+// Register
+app.post( '/', async ( { body }, res, next ) => {
+  try {
+    const hashedPassword = await bcrypt.hash( body.password, 10 )
+
+    // Save user credentials for auth document
+    const userAuth = new AuthSchema( {
+      email: body.email,
+      password: hashedPassword,
+      role: USER_ROLES.basic,
+    } )
+    const saveUserAuth = await userAuth.save()
+    const { _id: loginId } = saveUserAuth
+
+    // Save user User Document
+    const user = UserSchema( {
+      name: body.name,
+      organization: body.organization,
+      socialAccounts: body.socialAccounts,
+      login: loginId,
+    } )
+    const saveUser = await user.save()
+    const { _id: userId } = saveUser
+
+    // Link User document back to Auth Document (One to One relationship)
+    await AuthSchema.updateOne( { _id: saveUserAuth.id }, { $set: { user: userId } } )
+
+    return res.json( {
+      ...saveUser.toJSON(),
+      login: {
+        // Remove password from response
+        ...omit( saveUserAuth.toJSON(), 'password' ),
+      },
+    } )
+  } catch ( err ) { return next( err ) }
+} )
 
 export default app
