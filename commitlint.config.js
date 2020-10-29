6f6d1a172144720f1997c49839e54f04162c644a
saihaj <saihajpreet.singh@gmail.com>
2020-10-28 20:42:24 -0500

    build: setup commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+    scope: {
+      allowed: ["backend", "frontend", "docs"],
+    },
+    "body-max-line-length": [0],
+  },
+};
